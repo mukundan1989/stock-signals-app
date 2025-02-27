1c46c3f1bb5be672c007d9b2ef229098a4d57d78
mukundan1989 <128684032+mukundan1989@users.noreply.github.com>
2025-02-27 16:28:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -173,8 +173,7 @@ st.markdown(
 
     .metric-box-gain::before {
         content: "";
-        background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="%23ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 2v20M17 5H9.5a3.5 3.5 0 1 0 0 7h5a3.5 3.5 0 1 1 0 7H6"/></svg>');
-        background-size: 40px 40px;
+        background: linear-gradient(15deg, #000000, #232323) background-size: 40px 40px;
         background-position: left top;
         background-repeat: no-repeat;
         opacity: 0.3;
@@ -199,7 +198,7 @@ st.markdown(
 
     /* Third grid box with speedometer gauge icon */
     .metric-box-speedometer {
-        background: linear-gradient(135deg, var(--secondary-background-color), var(--background-color));
+        background: linear-gradient(15deg, #000000, #232323) background-size: 40px 40px;
         padding: 20px;
         border-radius: 10px;
         text-align: left;
@@ -240,7 +239,7 @@ st.markdown(
 
     /* Fourth grid box with dartboard and arrow icon */
     .metric-box-accuracy {
-        background: linear-gradient(135deg, var(--secondary-background-color), var(--background-color));
+        background: linear-gradient(15deg, #000000, #232323) background-size: 40px 40px;
         padding: 20px;
         border-radius: 10px;
         text-align: left;
