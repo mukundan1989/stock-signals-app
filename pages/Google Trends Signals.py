b2a277a6c6f3e203756ead3b5099b9c19876857b
mukundan1989 <128684032+mukundan1989@users.noreply.github.com>
2025-02-28 21:05:16 +0530

    Update Google Trends Signals.py

--- pages/Google Trends Signals.py
+++ pages/Google Trends Signals.py
@@ -12,12 +12,40 @@ st.markdown(
         color: var(--text-color);
     }
 
+    .logo {
+        width: 46px; /* Default size for desktop */
+        height: 46px;
+        margin-bottom: 2px; /* Spacing between logo and title */
+    }
+
+    /* Adjust logo size for smaller screens */
+    @media (max-width: 768px) {
+        .logo {
+            width: 32px; /* Smaller size for tablets */
+            height: 32px;
+        }
+    }
+
+    /* Adjust logo size for mobile devices */
+    @media (max-width: 480px) {
+        .logo {
+            width: 26px; /* Smallest size for mobile */
+            height: 26px;
+        }
+    }
+
+    .stHorizontalBlock {
+        background-color: black; /* Set background color to black */
+        border-radius: 20px; /* Adjust the border-radius for rounded edges */
+        padding: 10px 20px 20px 20px;
+    }
+
     .pretty-table {
         width: 100%;
         border-collapse: separate;
         border-spacing: 0 10px; /* Adjust spacing between rows */
         font-size: 0.9em;
-        font-family: sans-serif;
+        font-family: "Source Sans Pro", sans-serif;
         min-width: 400px;
         overflow: hidden;
         text-align: center;
@@ -27,14 +55,9 @@ st.markdown(
 
     /* Black background with curved edges for each row */
     .pretty-table tbody tr {
-        background-color: black; /* Set background color to black */
+        background-color: #161616; /* Set background color to black */
         border-radius: 20px; /* Adjust the border-radius for rounded edges */
         margin-bottom: 10px;
-    }
-
-    .pretty-table th {
-        background-color: #1c1c1c; /* Set background color to black */
-        color: #aeaeae !important; /* Set text color to light grey */
     }
 
     .pretty-table th:first-child {
@@ -43,13 +66,21 @@ st.markdown(
     }
 
     .pretty-table th:last-child {
-        border-top-right-radius: 30px; /* Adjust the border-radius for the last cell */
+        border-top-right-radius: 30px; /* Adjust the border-radius for the first cell */
         border-bottom-right-radius: 30px;
     }
 
-    /* Padding for table cells */
-    .pretty-table th, .pretty-table td {
+    .pretty-table th{
+        background-color: #000; /* Set background color to black */
+        color: #fff !important; /* Set background color to black */
         padding: 6px 9px;
+        text-align: center;
+        border: 2px solid #282828 !important; 
+    } 
+
+    /* Padding for table cells */
+    .pretty-table td {
+        padding: 5px 8px;
         text-align: center;
         border: none;
         border-top: 5px solid #282828 !important;
@@ -68,7 +99,7 @@ st.markdown(
 
     /* Hover effect for rows */
     .pretty-table tbody tr:hover {
-        background-color: #333; /* Darker shade for hover effect */
+        background-color: #000000; /* Darker shade for hover effect */
     }
 
     /* Ensure the text above the table is white */
@@ -76,21 +107,83 @@ st.markdown(
         color: var(--text-color) !important;
     }
 
+    /* Grid container for metric boxes */
+    .grid-container {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        gap: 10px;
+        justify-content: center;
+        align-items: center;
+    }
+
+    @media (max-width: 600px) {
+        .grid-container { grid-template-columns: repeat(2, 1fr); gap: 5px; }
+    }
+
+    /* Styling for the "Add Stock" button */
+    .stButton button {
+        background-color: var(--primary-color);
+        color: var(--text-color);
+        border-radius: 5px;
+        padding: 10px 20px;
+        font-size: 16px;
+        font-weight: bold;
+        border: none;
+        transition: background-color 0.3s ease;
+    }
+
+    .stButton button:hover {
+        background-color: var(--primary-hover-color);
+    }
+
+    /* Custom CSS for the Company Name cell */
+    .company-name-cell {
+        text-align: center;
+    }
+
+    .company-name-cell small {
+        background-color: #313131;
+        border-radius: 4px;
+        padding: 2px 6px;
+        color: var(--text-color);
+        display: inline-block;
+        margin-top: 4px;
+    }
+
     /* Custom CSS for the Sentiment cell */
     .sentiment-positive {
-        background-color: #2e7d32; /* Green background for positive sentiment */
+        background-color: #6eb330; /* Green background for positive sentiment */
         border-radius: 10px;
         padding: 10px;
         color: var(--text-color);
     }
 
     .sentiment-negative {
-        background-color: #c62828; /* Red background for negative sentiment */
+        background-color: #db503a; /* Red background for negative sentiment */
         border-radius: 10px;
         padding: 10px;
         color: var(--text-color);
     }
 
+    /* Custom CSS for aligning titles to the left */
+    .left-align {
+        text-align: left;
+        display: flex;
+        align-items: center;
+    }
+
+    .left-align svg {
+        margin-right: 10px;
+    }
+    
+    .st-b1 {
+        background-color: #000;
+    }
+
+    .st-bt {
+        background-color: #fff;
+    }
+    
     /* Custom CSS for the title with logo */
     .title-with-logo {
         display: flex;
@@ -164,7 +257,7 @@ def fetch_gtrend_signals():
 st.markdown(
     """
     <div class="title-with-logo">
-        <img src="https://raw.githubusercontent.com/mukundan1989/stock-signals-app/refs/heads/main/images/gtrend-logo.png" alt="Google Trends Logo">
+        <img src="https://raw.githubusercontent.com/mukundan1989/stock-signals-app/refs/heads/main/images/gtrend-logo.png" alt="Google Trends Logo" class="logo">
         <h1 style='text-align: center;'>Google Trends Signals</h1>
     </div>
     """,
