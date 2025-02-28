a1e303f73deb569019b0314e0eb0243c8194be05
mukundan1989 <128684032+mukundan1989@users.noreply.github.com>
2025-02-28 19:27:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -165,8 +165,8 @@ st.markdown(
 
     /* First grid box with line chart icon */
     .metric-box {
-        background: rgb(24,24,24);
-	background: radial-gradient(circle, rgba(24,24,24,1) 0%, rgba(0,0,0,1) 100%);
+        background: rgb(25,25,25);
+	background: radial-gradient(circle, rgba(25,25,25,1) 0%, rgba(13,13,13,1) 100%);
         padding: 20px;
         border-radius: 10px;
         text-align: left;
