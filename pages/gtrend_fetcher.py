c63422fb00791328c5b9a5ff47615855a47f545e
mukundan1989 <128684032+mukundan1989@users.noreply.github.com>
2025-03-21 19:23:21 +0530

    Update gtrend_fetcher.py

--- pages/gtrend_fetcher.py
+++ pages/gtrend_fetcher.py
@@ -11,7 +11,7 @@ from serpapi import GoogleSearch  # For fetching Google Trends data
 # Configuration
 API_KEY = "1ce12aafcdmshdb6eea1ac608501p1ab501jsn4fc681388b27"  # Your RapidAPI key for Llama API
 SERPAPI_KEY = "85f1efaedeed8b213c459d6973f27ba731ec82ab6612bad27a6e37ebd1164df1"  # Your SerpAPI key
-API_HOST = "meta-llama-3-8b.p.rapidapi.com"  # API host for Llama API
+API_HOST = "gpt-4o.p.rapidapi.com"  # API host for Llama API
 COMPANY_NAMES_FILE = "data/comp_names.txt"  # Path to the company names file
 KEYWORDS_OUTPUT_DIR = "/tmp/datagdata/output"  # Directory to save keyword CSV files
 TRENDS_OUTPUT_DIR = "/tmp/datagtrendoutputz/json/output"  # Directory to save Google Trends JSON files
