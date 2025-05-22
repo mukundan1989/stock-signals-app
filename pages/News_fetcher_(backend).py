ed356024d7c62261b8568dba15d636e404473ff0
mukundan1989 <128684032+mukundan1989@users.noreply.github.com>
2025-05-23 00:12:47 +0530

    Update News_fetcher_(backend).py

--- pages/News_fetcher_(backend).py
+++ pages/News_fetcher_(backend).py
@@ -158,7 +158,7 @@ def fetch_content_summary(title, publish_date):
 # Date input boxes
 col1, col2 = st.columns(2)
 with col1:
-    from_date = st.date_input("From Date", value=datetime(2025, 04, 01))
+    from_date = st.date_input("From Date", value=datetime(2025, 01, 01))
 with col2:
     to_date = st.date_input("To Date", value=datetime(2025, 04, 30))
 
